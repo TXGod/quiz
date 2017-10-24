13a9b057cd741062686abec0d0708d2551365ea1
Racibor <mustang@onet.eu>
2017-10-24 21:26:44 +0200

    Racibor
    Created structure

---
+++ src/main/java/entities/QuestionFactory.java
@@ -0,0 +1,5 @@
+package entities;
+
+public interface QuestionFactory {
+		public Question buildQuestion(int index);
+}
---
+++ src/main/java/entities/QuestionFactoryImpl.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class QuestionFactoryImpl implements QuestionFactory {
+
+	public Question buildQuestion(int index) {
+		return null;
+	}
+
+}
