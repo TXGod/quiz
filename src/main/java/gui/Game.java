4a48a96fde5333ad20658608eb77b6e23141e3a4
ragberr <33061692+ragberr@users.noreply.github.com>
2017-11-05 13:14:18 +0100

    Ragber

--- src/main/java/gui/Game.java
+++ src/main/java/gui/Game.java
@@ -1,6 +1,14 @@
 package gui;
 
+
+
 import entities.GameSession;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,6 +22,42 @@ public class Game {
 		this.mainMenu = mainMenu;
 		this.gameSession = gameSession;
 		this.root = (StackPane) root;
+		
+	}
+	
+	private void initGame() {
+		
+		Button right = new Button("Prawda") ;
+		right.setTranslateX(0);
+		right.setTranslateY(-200);
+		root.getChildren().add(right);
+		
+		Button wrong = new Button("Fa³sz");
+		wrong.setTranslateX(0);
+		wrong.setTranslateY(200);
+		root.getChildren().add(wrong);
+		
+		Label title = new Label("Tytu³");
+		wrong.setTranslateX(300);
+		wrong.setTranslateY(0);
+		root.getChildren().add(title);
+		
+		Label desc = new Label("Treœæ");
+		wrong.setTranslateX(200);
+		wrong.setTranslateY(0);
+		root.getChildren().add(desc);
+		
+		Label img = new Label("Obraz");
+		wrong.setTranslateX(300);
+		wrong.setTranslateY(0);
+		root.getChildren().add(img);
+		
+		Label score = new Label("Wynik");
+		wrong.setTranslateX(-100);
+		wrong.setTranslateY(200);
+		root.getChildren().add(score);
+		
+		
 		
 	}
 	
